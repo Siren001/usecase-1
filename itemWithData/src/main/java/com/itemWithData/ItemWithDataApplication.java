baa46c417a8c99724e7919632ee8675e03e5c2e4
Arya <Arya@DESKTOP-A1PA334>
2023-04-23 11:48:34 +0530

    new spring starter project - with dependancies

---
+++ itemWithData/src/main/java/com/itemWithData/ItemWithDataApplication.java
@@ -0,0 +1,13 @@
+package com.itemWithData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemWithDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemWithDataApplication.class, args);
+	}
+
+}
---
+++ itemWithData/src/test/java/com/itemWithData/ItemWithDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itemWithData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemWithDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
